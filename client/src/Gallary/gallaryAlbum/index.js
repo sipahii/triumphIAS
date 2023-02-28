7934eaa4d352774ef1edf3d913a01ff32ab52542
md imran ansari <mdimranitk@gmail.com>
2023-02-28 07:14:24 +0530

    complele gallary Page

--- client/src/Gallary/gallaryAlbum/index.js
+++ client/src/Gallary/gallaryAlbum/index.js
@@ -4,15 +4,23 @@ import Container from "../../component/Container";
 import GallaryImage from "./gallaryImage";
 
 const GallaryAlbume = () => {
-  const images = ["course1.jpg", "course2.jpg", "course3.jpg", "course4.jpg", "course5.jpg", "course6.jpg", "course7.jpg", "course8.jpg", "course4.jpg", "course1.jpg"];
+  // const images = ["course1.jpg", "course2.jpg", "course3.jpg", "course4.jpg", "course5.jpg", "course6.jpg", "course7.jpg", "course8.jpg", "course4.jpg", "course1.jpg", "course6.jpg", "course7.jpg"];
 
   return (
     <Container>
       <div className={Styles.gallaryAlbumArea}>
         <div className={Styles.gallaryAlbumArea__gallaryCard}>
-          {images.map((img, index) => {
-            return <GallaryImage key={index} imageSrc={img} />;
-          })}
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          <GallaryImage imageSrc="/course1.jpg" name="Imran" year="2003" catagory="IAS" />
+          {/* {images.map((img, index) => {
+            return <GallaryImage key={index} imageSrc={img} name="Imran" year="2003" catagory="IAS" />;
+          })} */}
         </div>
       </div>
     </Container>
